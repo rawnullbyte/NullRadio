0819436d785efbe08d731ede3b3fbabdd3c4e5c9
rawnullbyte <andrexyt2012@gmail.com>
2025-04-05 23:19:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def get_waifu_image(filename):
 
 # Start the app with Uvicorn
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
